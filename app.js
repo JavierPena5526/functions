c83e43fc72012811a0589219f8315bef885aa419
JavierPena5526 <javape5526@gmail.com>
2025-02-06 16:58:11 -0600

    added properties

---
+++ app.js
@@ -0,0 +1,32 @@
+
+// function toUpper(text) {
+//     const upperCased = text.toUpperCase();
+//     console.log(upperCased);
+// }
+
+// const toUpper = () =>{
+//     asdasd
+// }
+
+
+
+// function toCelsius(f) {
+//     console.log((5/9) * (f-32));
+// }
+
+// toCelsius(80);
+
+const name = "Javier";
+const age = 23;
+
+const person = {
+    firstName: "Javier",
+    lastName: "Pena",
+    age: "23"
+};
+
+let x = "firstName";
+let y = "age";
+console.log(
+    `hello it's me ${name} and my age is ${age}`);
+
